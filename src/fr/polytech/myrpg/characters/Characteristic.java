3bb1beaaf1e8ae5add8aa776da71eb0dc09ec144
DELORME Loïc <loic.delorme.pro@gmail.com>
2015-12-24 10:39:15 +0100

    Added characteristic class

---
+++ src/fr/polytech/myrpg/characters/Characteristic.java
@@ -0,0 +1,30 @@
+package fr.polytech.myrpg.characters;
+
+/**
+ * This enumeration represents all available characteristics.
+ *
+ * @author DELORME Loïc
+ * @since 1.0.0
+ */
+public enum Characteristic
+{
+    /**
+     * The strength of the character.
+     */
+	STRENGTH,
+
+	/**
+	 * The dexterity of the character.
+	 */
+	DEXTERITY,
+
+	/**
+	 * The defense of the character.
+	 */
+	DEFENSE,
+
+	/**
+	 * The health of the character.
+	 */
+	HEALTH;
+}
