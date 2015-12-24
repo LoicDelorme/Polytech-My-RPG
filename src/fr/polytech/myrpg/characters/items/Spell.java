6feeb74e02590f83e0a73ce9347f3c7aa5ff86b2
DELORME Loïc <loic.delorme.pro@gmail.com>
2015-12-24 10:35:53 +0100

    Added spell class

---
+++ src/fr/polytech/myrpg/characters/items/Spell.java
@@ -0,0 +1,54 @@
+package fr.polytech.myrpg.characters.items;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import fr.polytech.myrpg.characters.Characteristic;
+
+/**
+ * This class represents a spell.
+ *
+ * @author DELORME Loïc
+ * @since 1.0.0
+ */
+public class Spell extends Item
+{
+	/**
+	 * The list of available effects.
+	 */
+	private final List<Effect> effects;
+
+	/**
+	 * Create a spell.
+	 * 
+	 * @param name
+	 *            The name of the spell.
+	 * @param weight
+	 *            The weight of the spell.
+	 * @param effects
+	 *            The effects.
+	 */
+	public Spell(String name, int weight, Effect... effects)
+	{
+		super(name, weight, true, false);
+		this.effects = new ArrayList<Effect>(Arrays.asList(effects));
+	}
+
+	/**
+	 * @see fr.polytech.myrpg.characters.items.Item#getValueByCharacteristic(fr.polytech.myrpg.characters.Characteristic)
+	 */
+	@Override
+	public int getValueByCharacteristic(Characteristic characteristic)
+	{
+		for (Effect currentEffect : this.effects)
+		{
+			if (characteristic == currentEffect.getCharacteristic())
+			{
+				return currentEffect.getValue();
+			}
+		}
+
+		return 0;
+	}
+}
