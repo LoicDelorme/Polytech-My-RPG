1bbe165d8e2c2f462898ef9f2df5170d53c0b2b3
DELORME Loïc <loic.delorme.pro@gmail.com>
2015-12-24 11:29:57 +0100

    Added equipableitemtype enumeration

---
+++ src/fr/polytech/myrpg/characters/items/equipable/EquipableItemType.java
@@ -0,0 +1,20 @@
+package fr.polytech.myrpg.characters.items.equipable;
+
+/**
+ * This enumeration represents all available equipable item type.
+ *
+ * @author DELORME Loïc
+ * @since 1.0.0
+ */
+public enum EquipableItemType
+{
+    /**
+     * An armor.
+     */
+	ARMOR,
+
+	/**
+	 * A weapon.
+	 */
+	WEAPON;
+}
