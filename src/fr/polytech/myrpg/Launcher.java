50a208293edeeee6ec74559006f200a76c665eb6
DELORME Loïc <loic.delorme.pro@gmail.com>
2015-12-24 09:08:25 +0100

    Initial commit

---
+++ src/fr/polytech/myrpg/Launcher.java
@@ -0,0 +1,21 @@
+package fr.polytech.myrpg;
+
+/**
+ * The launcher of the application.
+ * 
+ * @author DELORME Loïc
+ */
+public class Launcher
+{
+
+	/**
+	 * The entry of the application.
+	 * 
+	 * @param args
+	 *            Some arguments.
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println("Coming soon...");
+	}
+}
