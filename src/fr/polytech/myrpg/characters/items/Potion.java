89e9447d4876c57dd3a19226a3f7c81d27fd0521
DELORME Loïc <loic.delorme.pro@gmail.com>
2015-12-24 10:34:28 +0100

    Added potion class

---
+++ src/fr/polytech/myrpg/characters/items/Potion.java
@@ -0,0 +1,47 @@
+package fr.polytech.myrpg.characters.items;
+
+import fr.polytech.myrpg.characters.Characteristic;
+
+/**
+ * This class represents a potion.
+ *
+ * @author DELORME Loïc
+ * @since 1.0.0
+ */
+public class Potion extends Item
+{
+	/**
+	 * The additional health.
+	 */
+	private final int additionalHealth;
+
+	/**
+	 * Create a potion.
+	 * 
+	 * @param name
+	 *            The name of the potion.
+	 * @param weight
+	 *            The weight of the potion.
+	 * @param additionalHealth
+	 *            The additional health.
+	 */
+	public Potion(String name, int weight, int additionalHealth)
+	{
+		super(name, weight, true, false);
+		this.additionalHealth = additionalHealth;
+	}
+
+	/**
+	 * @see fr.polytech.myrpg.characters.items.Item#getValueByCharacteristic(fr.polytech.myrpg.characters.Characteristic)
+	 */
+	@Override
+	public int getValueByCharacteristic(Characteristic characteristic)
+	{
+		if (characteristic == Characteristic.HEALTH)
+		{
+			return this.additionalHealth;
+		}
+
+		return 0;
+	}
+}
